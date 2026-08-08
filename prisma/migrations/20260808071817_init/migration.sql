759f5554d3f6ad80f6bd2602d6dfde1370aa5291
SepehrAhmadi <ahmadisepehr75@gmail.com>
2026-08-08 11:04:18 +0330

    Initialize Prisma client and migrate schema

---
+++ prisma/migrations/20260808071817_init/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE `Repository` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `owner` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `fullNmae` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `primaryLanguage` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Repository_fullNmae_key`(`fullNmae`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `GenerationJob` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `repositoryId` INTEGER NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `GeneratedReadme` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `jobId` INTEGER NOT NULL,
+    `content` LONGTEXT NOT NULL,
+    `totalTokens` INTEGER NOT NULL,
+    `modelUsed` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `GeneratedReadme_jobId_key`(`jobId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `GenerationJob` ADD CONSTRAINT `GenerationJob_repositoryId_fkey` FOREIGN KEY (`repositoryId`) REFERENCES `Repository`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `GeneratedReadme` ADD CONSTRAINT `GeneratedReadme_jobId_fkey` FOREIGN KEY (`jobId`) REFERENCES `GenerationJob`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
